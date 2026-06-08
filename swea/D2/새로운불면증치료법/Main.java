cfb58d0da7477504861a11ca1f1bd342f1544e6a
heau0105 <heau0105@naver.com>
2026-06-08 19:30:52 +0900

    2️⃣: [새로운불면증치료법]
    SWEA D2 - 새로운불면증치료법 풀이 추가

---
+++ swea/D2/새로운불면증치료법/Main.java
@@ -0,0 +1,34 @@
+package 새로운불면증치료법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=T;tc++) {
+			Integer originN = Integer.parseInt(br.readLine());
+
+			Integer num = 1;
+			Set<Character> s = new HashSet<>();
+			
+			while(true) {
+				Integer multipleN = originN * num;
+				String N = multipleN.toString();
+				int n = N.length();	
+				for(int i =0;i<n;i++) {
+					s.add(N.charAt(i));
+				}
+				if(s.size() == 10) {
+					System.out.println("#"+tc+" "+multipleN);
+					break;
+				}
+				num++;
+			}
+			
+		}
+	}
+}
